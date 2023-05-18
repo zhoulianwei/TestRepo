cc4384b8ef9c2873737720a759adb93115c09849
LeviZhou <Levi.Zhou@sbdinc.com>
2023-05-18 10:06:16 +0800

    Init CDK Project

---
+++ TestSolution/CDKTest/src/CdkTest/LeviTestStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace CdkTest
+{
+    public class LeviTestStack : Stack
+    {
+        internal LeviTestStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            
+        }
+    }
+}
---
+++ TestSolution/CDKTest/src/CdkTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CdkTest
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+
+            var enviroment = new Amazon.CDK.Environment
+            {
+                Account = "465671368404",
+                Region = "us-east-2",
+            };
+            var stackProp = new StackProps { Env = enviroment };
+
+            new LeviTestStack(app, "LeviTestStack", stackProp);
+
+            app.Synth();
+        }
+    }
+}
