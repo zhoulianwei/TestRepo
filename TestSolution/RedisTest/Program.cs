0c4c861dda218842475944415a9bca7efffbea77
Levi Zhou <Levi.Zhou@sbdinc.com>
2023-06-28 13:53:48 +0800

    Init redis project

---
+++ TestSolution/RedisTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisTest
+{
+    internal class Program
+    {
+        private int id { get; set; }
+
+        static void Main(string[] args)
+        {
+            var redisAddress = "localhost";
+            var muxer = ConnectionMultiplexer.Connect(redisAddress);
+            var redisDb = muxer.GetDatabase();
+            redisDb.StringSet("key1", "value1");
+            redisDb.StringSet("key2", "value2");
+            var isConnected = redisDb.IsConnected("123ABC");
+            Console.ReadKey();
+        }
+    }
+
+
+
+}
